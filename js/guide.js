c92d36ea433c78a93a0d456a5bc4dd317fc1f30f
iwest <bokthaku@gmail.com>
2022-11-06 13:17:27 -0500

    Add phoneme guide

--- js/guide.js
+++ js/guide.js
@@ -7,7 +7,8 @@
         ])
     ).done((modules, container, hashNav, fragment) => {
         var titles = {
-            install: 'How to Purchase, Download, and Install Synthesizer V Studio and Voice Databases'
+            install: 'How to Purchase, Download, and Install Synthesizer V Studio and Voice Databases',
+            phonemes: 'Entering lyrics & phonemes for better pronounciation & timing, and how to use dictionaries'
         }
 
         container('guide', {
