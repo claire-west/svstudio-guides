f6d9fd8f97e5e652e266a1537a98ed5d3b1d93f4
Claire West <bokthaku@gmail.com>
2024-01-13 23:10:31 -0500

    Add grunt watch for fragment preload

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,11 +31,22 @@ module.exports = function(grunt) {
         dest: 'js/',
         expand: true
       }
+    },
+    watch: {
+      testapp: {
+        files: [ 'frag/**/*.html', 'template/**' ],
+        tasks: [ 'preload' ],
+        options: {
+          spawn: false,
+        }
+      }
     }
   });
 
   grunt.loadTasks('./grunt');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.registerTask('default', [ 'build_templated_pages', 'generate_fragment_preload' ]);
+  grunt.registerTask('preload', [ 'generate_fragment_preload' ]);
   grunt.registerTask('prepare-neocities', [ 'build_templated_pages', 'generate_fragment_preload', 'copy' ]);
 };
